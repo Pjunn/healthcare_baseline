eaeeb35f7418c72191d50811c865b760ac244bfd
Pjunn <jsphilip54@gmail.com>
2023-12-12 18:23:41 +0900

    feat: use img_size in resize

--- datasets/augmentation.py
+++ datasets/augmentation.py
@@ -20,14 +20,14 @@ class BaseAugmentation(object):
     def get_transforms(self):
         if self.is_train:
             return transforms.Compose([
-                transforms.RandomResizedCrop((224,224)),
+                transforms.RandomResizedCrop((self.img_size,self.img_size)),
                 transforms.RandomRotation(30),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
         else:
             return transforms.Compose([
-                transforms.Resize((224,224)),
+                transforms.Resize((self.img_size,self.img_size)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
