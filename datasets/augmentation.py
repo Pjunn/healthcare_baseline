57fbe204ec3d3a515252a2f261b1fbff3db27096
Pjunn <jsphilip54@gmail.com>
2023-12-12 12:24:59 +0900

    feat: change resize param

--- datasets/augmentation.py
+++ datasets/augmentation.py
@@ -20,14 +20,14 @@ class BaseAugmentation(object):
     def get_transforms(self):
         if self.is_train:
             return transforms.Compose([
-                transforms.RandomResizedCrop(224),
+                transforms.RandomResizedCrop((224,224)),
                 transforms.RandomRotation(30),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
         else:
             return transforms.Compose([
-                transforms.Resize(224),
+                transforms.Resize((224,224)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
